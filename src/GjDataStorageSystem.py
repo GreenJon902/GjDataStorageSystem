a0ea3b78e4b75a07dcb04b0dc7e3694f5b1d073d
GreenJon902 <greenjon902@gmail.com>
2022-02-28 21:41:38 +0000

    setup kivy and betterlogger

---
+++ src/GjDataStorageSystem.py
@@ -0,0 +1,11 @@
+import os
+
+if __name__ == "__main__":
+    import betterLogger
+    base_logger = betterLogger.get_logger("BaseLogger")
+
+    base_logger.log_info("Setting up kivy")
+    os.environ["KIVY_NO_FILELOG"] = "True"
+    os.environ["KIVY_NO_CONSOLELOG"] = "True"
+    # noinspection PyUnresolvedReferences
+    import kivy
